cd68eba3eaf8ff271a2439136b146154b11754cb
Simon Olsson <simon.olsson@sogeti.com>
2019-11-25 13:59:26 +0100

    Add project files.

---
+++ Detecting/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Detecting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnDetecd = new System.Windows.Forms.Button();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(276, 517);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnDetecd
+            // 
+            this.btnDetecd.Location = new System.Drawing.Point(616, 517);
+            this.btnDetecd.Name = "btnDetecd";
+            this.btnDetecd.Size = new System.Drawing.Size(75, 23);
+            this.btnDetecd.TabIndex = 1;
+            this.btnDetecd.Text = "Detect";
+            this.btnDetecd.UseVisualStyleBackColor = true;
+            this.btnDetecd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // picImage
+            // 
+            this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picImage.Location = new System.Drawing.Point(48, 12);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(953, 499);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImage.TabIndex = 2;
+            this.picImage.TabStop = false;
+            this.picImage.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 543);
+            this.Controls.Add(this.picImage);
+            this.Controls.Add(this.btnDetecd);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnDetecd;
+        private System.Windows.Forms.PictureBox picImage;
+    }
+}
+
---
+++ Detecting/Form1.cs
@@ -0,0 +1,84 @@
+﻿using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Detecting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var configDetector = new ConfigurationDetector();
+            var config = configDetector.Detect();
+            var yolo = new YoloWrapper(config);
+            var memoryStream = new MemoryStream();
+            picImage.Image.Save(memoryStream, ImageFormat.Png);
+            var items = yolo.Detect(memoryStream.ToArray()).ToList();
+            AddDetailsToPictureBox(picImage, items);
+        }
+
+        void AddDetailsToPictureBox(PictureBox picBoxRender, List<YoloItem> items)
+        {
+            var img = picBoxRender.Image;
+
+            var font = new Font("Arial", 50, FontStyle.Regular);
+            var brush = new SolidBrush(Color.LightGreen);
+
+            var x1 = 1;
+            var y1 = 1;
+            var point1 = new Point(x1, y1);
+            int antal = items.Count;
+
+            var graphics = Graphics.FromImage(img);
+            foreach(var item in items)
+            {
+                
+                var x = item.X;
+                var y = item.Y;
+                var width = item.Width;
+                var height = item.Height;
+
+                var rect = new Rectangle(x, y, width, height);
+                var pen = new Pen(Color.LightGreen, 6);
+
+                var point = new Point(x, y);
+
+                graphics.DrawRectangle(pen, rect);
+                graphics.DrawString(item.Type, font, brush, point);
+                graphics.DrawString("Antal Objekt: " + antal, font, brush, point1);
+
+            }
+            picBoxRender.Image = img;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.png";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                picImage.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+    }
+}
